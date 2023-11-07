eca2dfe7e0e126341c2031f2988774b21b648649
ttoor5 <115154675+ttoor5@users.noreply.github.com>
2023-11-07 13:50:25 +0530

    Update astra_install.sh

--- astra_install.sh
+++ astra_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Starting installation Astra"
-wget -q http://cesbo.com/download/astra/4.4.182/free/x86_64/astra > /dev/null
+wget -q https://cdn.cesbo.com/astra/4.4.182-free/x86/linux-64bit/astra > /dev/null
 chmod +x astra > /dev/null
 sudo mv astra /usr/bin/ > /dev/null
 sudo mkdir /etc/astra > /dev/null
